49d403d58dd5a0b05c1aa3692e8472f07b3692f7
0gon <130747742+0gon@users.noreply.github.com>
2023-05-25 10:10:35 +0900

    원격에서 수정했습니다

--- 형상관리연습/src/형상관리연습/Hello.java
+++ 형상관리연습/src/형상관리연습/Hello.java
@@ -3,6 +3,7 @@ package 형상관리연습;
 public class Hello {
 	private void mian() {
 		// TODO Auto-generated method stub
+		//원격에서 작업했습니다
 		System.out.println("Hello world");
 		
 		System.out.println("변경");
